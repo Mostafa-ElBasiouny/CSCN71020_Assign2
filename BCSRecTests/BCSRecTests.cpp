826146dfcc6d6abc1f2ab3e896ff21855c2d91f3
Mostafa Elbasiouny <Melbasiouny4793@conestogac.on.ca>
2021-10-17 19:36:11 -0400

    Add unit tests for 'getPerimeter' and 'getArea'.

--- BCSRecTests/BCSRecTests.cpp
+++ BCSRecTests/BCSRecTests.cpp
@@ -1,6 +1,9 @@
 #include "pch.h"
 #include "CppUnitTest.h"
 
+extern "C" int getPerimeter(int* length, int* width);
+extern "C" int getArea(int* length, int* width);
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace BCSRecTests
@@ -8,9 +11,20 @@ namespace BCSRecTests
 	TEST_CLASS(BCSRecTests)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(getPerimeter_Valid_IfPerimeterIsCorrect)
 		{
+			int length = 32;
+			int width = 8;
+			int result = getPerimeter(&length, &width);
+			Assert::AreEqual(80, result);
+		}
+
+		TEST_METHOD(getArea_Valid_IfAreaIsCorrect)
+		{
+			int length = 16;
+			int width = 2;
+			int result = getArea(&length, &width);
+			Assert::AreEqual(32, result);
 		}
 	};
 }
